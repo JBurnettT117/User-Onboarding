5ba7047b8e350d70028300dee048f28ee35c00af
Omen <joshuaburnett429@gmail.com>
2022-12-19 21:26:59 -0600

    project completed

---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+    username: yup
+        .string()
+        .trim()
+        .required("username is required")
+        .min(3, "username must be atleast 3 charachters long"),
+    email: yup
+        .string()
+        .email("must be a valid email address")
+        .required("email is required"),
+    password: yup
+        .string()
+        .required("password is required")
+        .min(6, "atleast 6 charachters please"),
+    tos: yup
+        .boolean()
+        .oneOf([true], "must accept terms and conditions")
+})
+
+export default formSchema;
